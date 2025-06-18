e6f01796bcc9cea002cb8b3424e5e61a4e3be700
Rakhib Kadirov <RahiBihar@yandex.ru>
2025-06-19 01:26:57 +0300

    first

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,88 @@
+"use client"
+
 import Image from "next/image";
 import styles from "./page.module.css";
+import Header from "./components/header";
+import Reviews from "./components/Reviews/reviews";
+import AddProduct from "./components/AddProduct";
+import { getProducts } from '@/api/route'
+import { useCallback, useEffect, useRef, useState } from "react";
+import { Basket } from "./components/AddProduct/basket";
+
+interface Product {
+	page: number;
+	amount: number;
+	total: number;
+	items: ProductItem[]
+}
+
+interface ProductItem {
+	id: number;
+	image_url: string;
+	title: string;
+	description: string;
+	price: number;
+}
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+	const [productState, setProductState] = useState<ProductItem[]>([])
+	const [pageState, setPageState] = useState(1)
+	const [totalState, setTotalState] = useState(0)
+	const [isLoading, setIsLoading] = useState(false)
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+	const observer = useRef<IntersectionObserver>(null)
+	const sentinelRef = useRef(null);
+
+	const loadProducts = useCallback(async () => {
+		if (isLoading || pageState * 6 > totalState) return
+
+		setIsLoading(true)
+		const newProducts = await getProducts(pageState, 6)
+		setProductState(prev => [...prev, ...newProducts.items])
+		setTotalState(newProducts.total)
+		setIsLoading(false)
+	}, [isLoading, pageState, totalState])
+
+	useEffect(() => {
+		const fetchData = async () => {
+			const [dataProducts] = await Promise.all([getProducts(1, 6)])
+			setProductState(dataProducts.items)
+			setTotalState(dataProducts.total)
+		}
+		fetchData()
+	}, [])
+
+	useEffect(() => {
+		const fetchProducts = async () => {
+			await loadProducts()
+		}
+		fetchProducts()
+	}, [pageState])
+
+	useEffect(() => {
+		const callback = (entries: any) => {
+			if (entries[0].isInteresting && !isLoading) setPageState(prev => prev + 1)
+		}
+
+		observer.current = new IntersectionObserver(callback)
+		const currentSentinel = sentinelRef.current
+		if (currentSentinel) observer.current.observe(currentSentinel)
+
+		return () => { if (currentSentinel) observer.current?.unobserve(currentSentinel) }
+	}, [isLoading])
+
+	return (
+		<div className="flex min-h-screen flex-col items-center justify-start gap-32">
+			<Header />
+			<main className='w-[1442px] flex flex-col justify-center items-center'>
+				<Reviews />
+				<Basket />
+				<div className='grid grid-cols-[1fr_1fr_1fr] gap-[20px] sm:grid-cols-3 mt-[100px]'>
+					{productState.map(product => (
+						<AddProduct key={product.id} product={product} />
+					))}
+				</div>
+			</main>
+		</div>
+	);
 }
